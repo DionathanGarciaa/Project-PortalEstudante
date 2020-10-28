b02e2529158fa9347e29e18102dbc14c4e1dcc5d
Eduarda Rosa <eduardarsantos99@gmail.com>
2020-10-28 08:50:16 -0300

    finalizado

--- src/Pages/Matricula/index.js
+++ src/Pages/Matricula/index.js
@@ -1,19 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from '../../components/Header';
 import { Exit, Container, Selects, Button } from './style';
 import { Link } from 'react-router-dom';
 import { BsBoxArrowInLeft } from 'react-icons/bs';
-
+import Api from '../../services/Api';
 
 const Matricula = () => {
     const [matricula, setMatricula] = useState();
-    const [disciplinas, setDisciplinas] = useState([{nome: "Português"}]);
-    const [disciplina1, setDisciplina1] = useState();
-    const [disciplina2, setDisciplina2] = useState();
-    const [disciplina3, setDisciplina3] = useState();
-    const [disciplina4, setDisciplina4] = useState();
-    const [disciplina5, setDisciplina5] = useState();
-    const [disciplina6, setDisciplina6] = useState();
+    const [disciplinas, setDisciplinas] = useState([]);
+    const [disciplinesSelected, setDisciplinesSelected]= useState([
+       '', '', '', '', '', ''
+    ])
+
+
+    useEffect(() => {
+        Api.get('/content').then((response) => {
+            setDisciplinas(response.data.discipline)
+        })
+    }, [])
+ 
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        cadastrarDisciplinas()
+    }
+
+    function cadastrarDisciplinas () {
+        // let materia = [disciplina1, disciplina2, disciplina3, disciplina4, disciplina5, disciplina6];
+
+
+        Api.post(`/registration/${matricula}`, {disciplines: disciplinesSelected}).then((response) => {
+            console.log(response.data)
+            alert("ok")
+    } )
+}
+
+
 
     return (
         <>
@@ -31,7 +53,7 @@ const Matricula = () => {
             <Container>
            
                 {/* FORMULÁRIO */}
-                <form>
+                <form onSubmit={handleSubmit}>
 
                     {/* INPUT N° MATRICULA ALUNO */}
                     <input 
@@ -48,77 +70,100 @@ const Matricula = () => {
                     <Selects>
                         <select 
                             id="disciplinas"
-                            value={disciplina1}
-                            onChange={(event) => setDisciplina1(event.target.value)}
+                            value={disciplinesSelected[0]}
+                            onChange={(event) =>{
+                                let disciplines = [...disciplinesSelected];
+                                disciplines[0]=event.target.value;
+                                setDisciplinesSelected(disciplines)}}
+                                
                         >
                             <option value="" disabled selected>Disciplina</option>
                             {disciplinas.map((disciplina)=>(
-                                <option value={disciplina.nome}>{disciplina.nome}</option>
+                                    <option value={disciplina._id}>{disciplina.name}</option>
                             ))}
                         </select>
 
                         <select 
                             id="disciplinas"
-                            value={disciplina2}
-                            onChange={(event) => setDisciplina2(event.target.value)}
+                            value={disciplinesSelected[1]}
+                            onChange={(event) =>{
+                                let disciplines = [...disciplinesSelected];
+                                disciplines[1]=event.target.value;
+                                setDisciplinesSelected(disciplines)}}
                         >
                             <option value="" disabled selected>Disciplina</option>
                             {disciplinas.map((disciplina)=>(
-                                <option value={disciplina.nome}>{disciplina.nome}</option>
+                                <option value={disciplina._id}>{disciplina.name}</option>
                             ))}
                         </select>
 
                         <select 
                             id="disciplinas"
-                            value={disciplina3}
-                            onChange={(event) => setDisciplina3(event.target.value)}
+                            value={disciplinesSelected[2]}
+                            onChange={(event) =>{
+                                let disciplines = [...disciplinesSelected];
+                                disciplines[2]=event.target.value;
+                                setDisciplinesSelected(disciplines)}}
+
                         >
                             <option value="" disabled selected>Disciplina</option>
                             {disciplinas.map((disciplina)=>(
-                                <option value={disciplina.nome}>{disciplina.nome}</option>
+                                <option value={disciplina._id}>{disciplina.name}</option>
                             ))}
                         </select>
 
                         <select 
                             id="disciplinas"
-                            value={disciplina4}
-                            onChange={(event) => setDisciplina4(event.target.value)}
+                            value={disciplinesSelected[3]}
+                            onChange={(event) =>{
+                                let disciplines = [...disciplinesSelected];
+                                disciplines[3]=event.target.value;
+                                setDisciplinesSelected(disciplines)}}
+
                         >
                             <option value="" disabled selected>Disciplina</option>
                                 {disciplinas.map((disciplina)=>(
-                                <option value={disciplina.nome}>{disciplina.nome}</option>
+                                <option value={disciplina._id}>{disciplina.name}</option>
                             ))}
                         </select>
 
                         <select 
                             id="disciplinas"
-                            value={disciplina5}
-                            onChange={(event) => setDisciplina5(event.target.value)}
+                            value={disciplinesSelected[4]}
+                            onChange={(event) =>{
+                                let disciplines = [...disciplinesSelected];
+                                disciplines[4]=event.target.value;
+                                setDisciplinesSelected(disciplines)}}
+
                         >
                             <option value="" disabled selected>Disciplina</option>
                             {disciplinas.map((disciplina)=>(
-                                <option value={disciplina.nome}>{disciplina.nome}</option>
+                                <option value={disciplina._id}>{disciplina.name}</option>
                             ))}
                         </select>
 
                         <select 
                             id="disciplinas"
-                            value={disciplina6}
-                            onChange={(event) => setDisciplina6(event.target.value)}
+                            value={disciplinesSelected[5]}
+                            onChange={(event) =>{
+                                let disciplines = [...disciplinesSelected];
+                                disciplines[5]=event.target.value;
+                                setDisciplinesSelected(disciplines)}}
+
                         >
                             <option value="" disabled selected>Disciplina</option>
                             {disciplinas.map((disciplina)=>(
-                                <option value={disciplina.nome}>{disciplina.nome}</option>
+                                <option value={disciplina._id}>{disciplina.name}</option>
                             ))}
                         </select>
                     </Selects>
+            <Button>
+                <button type="submit" >Salvar</button>
+            </Button>
                 </form>
             </Container>
 
             {/* BOTÃO */}
-            <Button>
-                <button>Salvar</button>
-            </Button>
         </>
     )
 }
