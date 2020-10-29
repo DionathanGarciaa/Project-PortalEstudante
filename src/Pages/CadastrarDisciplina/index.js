523c2c0cd160915193cbcc9f3d6c4fd322b3416b
Benhur <benhur_zambiasi@hotmail.com>
2020-10-29 18:57:05 -0300

    incluindo o token nas erquizições

--- src/Pages/CadastrarDisciplina/index.js
+++ src/Pages/CadastrarDisciplina/index.js
@@ -15,7 +15,7 @@ const Disciplina = (props) => {
     const [errorMessage, setErrorMessage] = useState(false);
 
 
-    function ModalClickSuccess(){
+    function ModalClickSuccess() {
         setModalAlertSuccess(false);
     }
 
@@ -25,8 +25,10 @@ const Disciplina = (props) => {
     }
 
     function createDiscipline() {
-        Api.post("/discipline", { name: nome, idTeacher: matricula, numberStudents: alunos }).then(res => {
-            if(res.data){
+        Api.post("/discipline", { name: nome, idTeacher: matricula, numberStudents: alunos }, {
+            headers: { Authorization: 'Bearer ' + sessionStorage.getItem('token') }
+        }).then(res => {
+            if (res.data) {
                 setModalAlertSuccess(true)
             }
         }, (err) => {
@@ -36,11 +38,11 @@ const Disciplina = (props) => {
     }
 
     return (
-        
+
         <>
             {/* MODAL */}
-            {modalAlertSuccess && <AlertSuccess showAlertSuccess={ModalClickSuccess} text={"Disciplina cadastrada"}/>}
-            {modalAlertErro && <AlertErro showAlertErro={setModalAlertErro} text={errorMessage}/>}
+            {modalAlertSuccess && <AlertSuccess showAlertSuccess={ModalClickSuccess} text={"Disciplina cadastrada"} />}
+            {modalAlertErro && <AlertErro showAlertErro={setModalAlertErro} text={errorMessage} />}
 
             <Container>
 
@@ -48,7 +50,7 @@ const Disciplina = (props) => {
 
                     {/* SAIR  */}
                     <Exit onClick={() => props.showModal2(false)}>
-                        <BsX fontSize={30} color="red"/>
+                        <BsX fontSize={30} color="red" />
                     </Exit>
 
                     {/* TITULO */}
@@ -60,7 +62,7 @@ const Disciplina = (props) => {
                     <form onSubmit={handleSubmit}>
 
                         {/* INPUT NOME */}
-                        <input 
+                        <input
                             id="nome"
                             type="text"
                             placeholder="Nome"
@@ -68,9 +70,9 @@ const Disciplina = (props) => {
                             onChange={(event) => setNome(event.target.value)}
                             required
                         />
-                            
+
                         {/* INPUT MATRICULA PROFESSOR RESPONSAVEL */}
-                        <input 
+                        <input
                             id="matricula"
                             type="text"
                             placeholder="Matrícula Professor Responsável"
@@ -80,7 +82,7 @@ const Disciplina = (props) => {
                         />
 
                         {/* INPUT QUANTIDADE DE ALUNOS */}
-                        <input 
+                        <input
                             id="alunos"
                             type="text"
                             placeholder="Quantidade de Alunos"
@@ -93,12 +95,12 @@ const Disciplina = (props) => {
                         <Button>
                             <button>Salvar</button>
                         </Button>
-                                
+
                     </form>
                 </Formulario>
             </Container>
         </>
-    
+
     )
 }
 
--- src/Pages/LoginAlunoProfessor/index.js
+++ src/Pages/LoginAlunoProfessor/index.js
@@ -1,45 +1,45 @@
 import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { Container, Title, Formulario, Check, CheckBox, Box1, Box2, Senha, Botao, Imagem } from './style';
-import Img from '../../assets/img2.svg';
-// import Api from '../../services/Api';
-// import Alert from '../../components/ModalAlerts/ErroAlert';
+import Img from '../../Assets/img2.svg';
+import Api from '../../services/Api';
+import Alert from '../../components/ModalAlerts/ErroAlert';
 
 
 const MeuPortal = () => {
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
-    const [checkboxEstudante, setCheckboxEstudante] = useState('');
-    const [checkboxProfessor, setCheckboxProfessor] = useState('');
-    // const [modalAlertErro, setModalAlertErro] = useState(false);
-    // const [errorMessage, setErrorMessage] = useState(false);
+    const [checkbox, setCheckbox] = useState('');
+    const [modalAlertErro, setModalAlertErro] = useState(false);
+    const [errorMessage, setErrorMessage] = useState(false);
     const history = useHistory();
 
 
-    
+
     function handleSubmit(event) {
         event.preventDefault();
-        // loginUser();
+        loginUser();
     }
 
-    // function loginUser() {
-    //     Api.post("/sessions", {email: email, password: senha}).then(response => {
-    //         if(response.data.token){
-    //             sessionStorage.setItem("token", response.data.token)
-    //             history.push("/Home")
-    //         }
-    //     }, (err) => {
-    //         setErrorMessage(err.response.data.error);
-    //         setModalAlertErro(true)
-    //     })
-    // }
+    function loginUser() {
+        Api.post("/sessions", { email: email, password: senha }).then(response => {
+            if (response.data.token) {
+                sessionStorage.setItem("token", response.data.token)
+                sessionStorage.setItem("check", checkbox)
+                history.push("/Home")
+            }
+        }, (err) => {
+            setErrorMessage(err.response.data.error);
+            setModalAlertErro(true)
+        })
+    }
 
     return (
 
         <>
             {/* MODAL */}
-            {/* {modalAlertErro && <Alert showAlertErro={setModalAlertErro} text={errorMessage}/>} */}
-            
+            {modalAlertErro && <Alert showAlertErro={setModalAlertErro} text={errorMessage} />}
+
             <Container>
 
                 {/* TITULO */}
@@ -55,54 +55,29 @@ const MeuPortal = () => {
                         <Check>
 
                             <CheckBox>
-                                <input 
-                                    className="Box1"
-                                    id="checkbox"
-                                    type="checkbox"
-                                    value={checkboxEstudante}
-                                    onChange={(event) => setCheckboxEstudante(event.target.value)}
-                                /> 
+                                <input className="Box1" id="checkbox" type="checkbox" value="3" onChange={(event) => setCheckbox(event.target.value)} />
                                 <label htmlFor="checkbox">Estudante</label>
                             </CheckBox>
 
 
                             {/* CHECK BOX PROFESSOR */}
                             <CheckBox>
-                                    <input
-                                    className="Box1" 
-                                    id="checkbox"
-                                    type="checkbox"
-                                    value={checkboxProfessor}
-                                    onChange={(event) => setCheckboxProfessor(event.target.value)}
-                                    /> 
+                                <input className="Box1" id="checkbox" type="checkbox" value="2" onChange={(event) => setCheckbox(event.target.value)} />
                                 <label htmlFor="checkbox">Professor</label>
-                                </CheckBox>
-                        
+                            </CheckBox>
+
                         </Check>
 
                         {/* INPUT EMAIL */}
                         <Box1>
                             <label htmlFor="Email">E-mail</label>
-                            <input 
-                                id="Email"
-                                Type="email"
-                                Value={email}
-                                onChange={(event) => setEmail(event.target.value)}
-                                required
-                            />
+                            <input id="Email" type="email" value={email} onChange={(event) => setEmail(event.target.value)} required />
                         </Box1>
 
                         {/* INPUT SENHA */}
                         <Box2>
                             <label htmlFor="Senha">Senha</label>
-                            <input 
-                                id="Senha"
-                                Type="password"
-                                Value={senha}
-                                onChange={(event) => setSenha(event.target.value)}
-                                minLength='6'
-                                required
-                            />
+                            <input id="Senha" type="password" value={senha} onChange={(event) => setSenha(event.target.value)} minLength='6' required />
                         </Box2>
 
                         {/* LINK PARA REDEFINIR SENHA */}
@@ -123,7 +98,7 @@ const MeuPortal = () => {
 
                 {/* IMAGEM */}
                 <Imagem>
-                <img src={Img} alt='ilustração de alguém caminhando'></img>
+                    <img src={Img} alt='ilustração de alguém caminhando'></img>
                 </Imagem>
 
             </Container>
