0a21a22d45ba9526c8ebdc103c58f52fce56a697
Lucas B. Teixeira <36702465+lucasbetx@users.noreply.github.com>
2020-11-03 14:14:52 -0300

    CadastroVisualizarConteudo

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -10,6 +10,8 @@ import CadastrarDisciplina from '../Pages/CadastrarDisciplina';
 import MeuPortal from '../Pages/LoginAlunoProfessor';
 import RedefinirAlunoProfessor from '../Pages/RedefinirSenhaAlunoProfessor';
 import CadastroNota from '../Pages/CadastroNota';
+import CadastroConteudo from '../Pages/CadastroConteudo';
+import VisualizarConteudo from '../Pages/VisualizarConteudo';
 
 function Routes() {
     return (
@@ -24,6 +26,8 @@ function Routes() {
             <Route path="/MeuPortal" component={MeuPortal} />
             <Route path="/RedefinirSenhaAlunoProfessor" component={RedefinirAlunoProfessor} />
             <Route path="/CadastroNota" component={CadastroNota} />
+            <Route path="/CadastroConteudo" component={CadastroConteudo} />
+            <Route path="/VisualizarConteudo" component={VisualizarConteudo} />
         </Switch>
     );
 }
